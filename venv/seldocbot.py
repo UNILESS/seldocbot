ae46d7651971b5bd4483e85119df0330fd128e90
UNILESS <44114950+UNILESS@users.noreply.github.com>
2021-03-08 10:37:24 +0900

    3/8

--- venv/seldocbot.py
+++ venv/seldocbot.py
@@ -17,12 +17,39 @@ driver = webdriver.Chrome(executable_path='C:/Users/sunup/PycharmProjects/seldoc
 driver.get('http://freeforms.co.kr')
 driver.implicitly_wait(3)
 
-tag_names = driver.find_element_by_css_selector(".contents_list").find_elements_by_tag_name("a")
+tag_names = driver.find_element_by_id("top-memu-wrap").find_elements_by_tag_name("a")
+i = 1
 for tag in tag_names:
-    print(tag.text.split("\n"))
+    print(i,"번", tag.text.split("\n"))
+    i += 1
 
-user_input = quote_plus(input('원하는 서식 번호를 입력해주세요'))
-time.sleep(3)
+user_input = int(quote_plus(input('원하는 서식 번호를 입력해주세요')))
+
+if user_input == 1:
+    url = "/form100/form_1.html"
+elif user_input == 2:
+    url = "/form104/form_1.html"
+elif user_input == 3:
+    url = "/form110/form_1.html"
+elif user_input == 4:
+    url = "/form116/form_1.html"
+elif user_input == 5:
+    url = "/form120/form_1.html"
+elif user_input == 6:
+    url = "/form130/form_1.html"
+elif user_input == 7:
+    url = "/form140/form_1.html"
+elif user_input == 8:
+    url = "/form200/form_1.html"
+elif user_input == 9:
+    url = "/form210/form_1.html"
+elif user_input == 10:
+    url = "/form220/form_1.html"
+elif user_input == 11:
+    url = "/form230/form_1.html"
+
+
+url = f'http://freeforms.co.kr{url}'
 
 '''try:  # 정상 처리
     element = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, 'subject')))
