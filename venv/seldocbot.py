a25560aff1bc899b02331621ba8ab5d094afc649
UNILESS <44114950+UNILESS@users.noreply.github.com>
2021-03-09 13:51:26 +0900

    sample 최종완성

--- venv/seldocbot.py
+++ venv/seldocbot.py
@@ -17,7 +17,8 @@ options.add_argument('disable-gpu')
 options.add_argument('lang=ko_KR')
 
 options.add_experimental_option("prefs", {
-    "download.default_directory": r"C:/Users/sunup/Desktop/docdown",
+    "download.default_directory":
+        r"C:/Users/sunup/Desktop/docdown",
     "download.prompt_for_download": False,
     "download.directory_upgrade": True,
     "safebrowsing.enabled": True
@@ -27,44 +28,26 @@ driver = webdriver.Chrome(executable_path='C:/Users/sunup/PycharmProjects/seldoc
 driver.get('http://freeforms.co.kr')
 driver.implicitly_wait(3)
 
-
-
 tag_names = driver.find_element_by_id("top-memu-wrap").find_elements_by_tag_name("a")
 i = 1
+print("******서식******")
 for tag in tag_names:
-    print(i,"번", tag.text.split("\n"))
+    print(i,".", tag.text.split("\n"))
     i += 1
 
-user_input = int(quote_plus(input('\n원하는 서식 번호를 입력해주세요 : ')))
+user_input = int(quote_plus(input('\n원하는 서식 번호를 입력해주세요: ')))
 
-if user_input == 1:
-    url = "/form100/form_1.html"
-elif user_input == 2:
-    url = "/form104/form_1.html"
-elif user_input == 3:
-    url = "/form110/form_1.html"
-elif user_input == 4:
-    url = "/form116/form_1.html"
-elif user_input == 5:
-    url = "/form120/form_1.html"
-elif user_input == 6:
-    url = "/form130/form_1.html"
-elif user_input == 7:
-    url = "/form140/form_1.html"
-elif user_input == 8:
-    url = "/form200/form_1.html"
-elif user_input == 9:
-    url = "/form210/form_1.html"
-elif user_input == 10:
-    url = "/form220/form_1.html"
-elif user_input == 11:
-    url = "/form230/form_1.html"
+html = driver.page_source
 
+soup = BeautifulSoup(html, 'html.parser')
 
-url = f'http://freeforms.co.kr{url}'
+for href in soup.select('#top-memu-wrap'):
+    doctypeurl = href.findAll("a")[user_input - 1]['href'] # 0부터시작
+    # //*[@id="top-memu-wrap"]/a[1]
+
+url = f'http://freeforms.co.kr{doctypeurl}'
 
 driver.get(url)
-
 
 try:  # 정상 처리
     element = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, 'title')))
@@ -72,9 +55,9 @@ try:  # 정상 처리
     link = []
     pageNum = len(driver.find_element_by_tag_name("li").find_elements_by_class_name("page_box"))
     print("총", pageNum, "페이지 입니다.")
-    print("현재 1페이지 입니다.")
+    print("현재 1 페이지 입니다.")
 
-    j = 0
+    j = 1
 
     for i in range(0, pageNum):
         doc_data = driver.find_elements_by_class_name('title') # 0
@@ -82,21 +65,17 @@ try:  # 정상 처리
         webpage = requests.get(url)
         soup = BeautifulSoup(webpage.content, "html.parser")
 
-        # print(len(doc_data))
-
         for k in doc_data:
             doc_list.append(k.text.split('\n'))
 
-
-        num = 1
         i = 0
+
         for href in soup.select(".contents_list-2"):
             new_url = "http://freeforms.co.kr" + href.find("a")["href"]
             link.append("http://freeforms.co.kr" + href.find("a")["href"])
             print(new_url)
             for name_href in soup.select(".contents_list"): # in 2
-                # driver.get(new_url) 다운
-                # driver.implicitly_wait(1) waiting
+                # driver.get(new_url) 다운로드 코드 주석
                 name = soup.select(".contents_list-1 > a")[i].text
                 # // *[ @ id = "content"] / div[4] / div[1] / a
                 # //*[@id="content"]/div[4]/div[2]/a
@@ -107,39 +86,15 @@ try:  # 정상 처리
             # //*[@id="content"]/div[4]/div[2]/a
 
         time.sleep(2)  # 웹페이지를 불러오기 위해 2초 정지
-        # //*[@id="content"]/div[6]/ul/a[1]
-        j += 1
 
         if (j > pageNum):
             break
 
-        if user_input == 1:
-            url = "/form100/form_"+str(j)+".html"
-        elif user_input == 2:
-            url = "/form104/form_"+str(j)+".html"
-        elif user_input == 3:
-            url = "/form110/form_"+str(j)+".html"
-        elif user_input == 4:
-            url = "/form116/form_"+str(j)+".html"
-        elif user_input == 5:
-            url = "/form120/form_"+str(j)+".html"
-        elif user_input == 6:
-            url = "/form130/form_"+str(j)+".html"
-        elif user_input == 7:
-            url = "/form140/form_"+str(j)+".html"
-        elif user_input == 8:
-            url = "/form200/form_"+str(j)+".html"
-        elif user_input == 9:
-            url = "/form210/form_"+str(j)+".html"
-        elif user_input == 10:
-            url = "/form220/form_"+str(j)+".html"
-        elif user_input == 11:
-            url = "/form230/form_"+str(j)+".html"
-
-        url = f'http://freeforms.co.kr{url}'
+        url = url[:36] + str(j) + '.html'
         driver.get(url)
         print("현재", j, "페이지 다운로드를 마쳤습니다.")
 
+        j += 1
         # //*[@id="content"]/div[6]/ul/a[1]
 
 except TimeoutException:  # 예외 처리
@@ -148,7 +103,7 @@ except TimeoutException:  # 예외 처리
 finally:  # 정상, 예외 둘 중 하나여도 반드시 실행
     driver.quit()
 
-print("doc_list : ", len(doc_list))
+print("총 다운받은 문서의 개수:", len(doc_list))
 
 doc_df = pd.DataFrame({'문서명': doc_list, 'URL': link})
 doc_df = pd.DataFrame(zip(doc_list, link), columns=['문서명', 'URL'])
