3769727aa402eb6f7dc4fb5d8fe96c38d1a6cf2e
UNILESS <44114950+UNILESS@users.noreply.github.com>
2021-03-10 17:49:36 +0900

    sample 3/10 완성

--- venv/seldocbot.py
+++ venv/seldocbot.py
@@ -9,7 +9,7 @@ import time
 import pandas as pd
 import requests
 
-# 크롬 옵션
+# options about Chrome
 options = webdriver.ChromeOptions()
 options.add_argument('--incognito')
 options.headless = True
@@ -33,7 +33,7 @@ tag_names = driver.find_element_by_id("top-memu-wrap").find_elements_by_tag_name
 i = 1
 print("******서식******")
 for tag in tag_names:
-    print(i,".", tag.text.split("\n"))
+    print(str(i)+".", tag.text.split("\n"))
     i += 1
 
 user_input = int(quote_plus(input('\n원하는 서식 번호를 입력해주세요: ')))
@@ -46,7 +46,7 @@ html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
 for href in soup.select('#top-memu-wrap'):
-    doctypeurl = href.findAll("a")[user_input - 1]['href'] # 0부터시작
+    doctypeurl = href.findAll("a")[user_input - 1]['href']
     # //*[@id="top-memu-wrap"]/a[1]
 
 url = f'http://freeforms.co.kr{doctypeurl}'
@@ -55,7 +55,7 @@ driver.get(url)
 
 j = 0
 
-try:  # 정상 처리
+try:  # normal
     ten = 0
     element = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, 'title')))
     doc_list = []
@@ -64,7 +64,7 @@ try:  # 정상 처리
 
     while True:
         if len(driver.find_elements_by_class_name("page_box_b")) == 0:
-            # print(len(driver.find_element_by_tag_name("li").find_elements_by_class_name("page_box")))
+            # print(len(driver.find_element_by_tag_name("li").find_elements_by_class_name("page_box"))) to debug
             element = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, 'title')))
             pageNum = len(driver.find_element_by_tag_name("ul").find_elements_by_class_name("page_box"))
             break
@@ -77,7 +77,7 @@ try:  # 정상 처리
         driver.get(url)
         pageNum = len(driver.find_element_by_tag_name("ul").find_elements_by_class_name("page_box"))
         if pageNum <=9:
-            # print(len(driver.find_element_by_tag_name("li").find_elements_by_class_name("page_box")))
+            # print(len(driver.find_element_by_tag_name("li").find_elements_by_class_name("page_box"))) to debug
             for i in range(2 , len(driver.find_element_by_tag_name("li").find_elements_by_class_name("page_box"))):
                 element = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, 'title')))
                 pageNum = len(driver.find_element_by_tag_name("ul").find_elements_by_class_name("page_box"))
@@ -108,8 +108,8 @@ try:  # 정상 처리
             link.append("http://freeforms.co.kr" + href.find("a")["href"])
             print(new_url)
             for name_href in soup.select(".contents_list"):
-                # driver.get(new_url) # 다운
-                # time.sleep(1) DDoS 방지용
+                # driver.get(new_url) # to download
+                # time.sleep(1) DDoS warning
                 name = soup.select(".contents_list-1 > a")[i].text
                 # // *[ @ id = "content"] / div[4] / div[1] / a
                 # //*[@id="content"]/div[4]/div[2]/a
@@ -119,7 +119,7 @@ try:  # 정상 처리
                     break
             # //*[@id="content"]/div[4]/div[2]/a
 
-        time.sleep(1)  # 웹페이지를 불러오기 위해 2초 정지
+        time.sleep(1)  # DDoS warning
 
         if (j > ten * 10 + pageNum):
             break
@@ -131,10 +131,10 @@ try:  # 정상 처리
         j += 1
         # //*[@id="content"]/div[6]/ul/a[1]
 
-except TimeoutException:  # 예외 처리
+except TimeoutException:  # exception
     print('해당 페이지에 문서가 존재하지 않습니다.')
 
-finally:  # 정상, 예외 둘 중 하나여도 반드시 실행
+finally:  # must be executed
     driver.quit()
 
 print("총 다운받은 문서의 개수:", len(doc_list))
